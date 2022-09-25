44516c9ed4af4f7257d12662f942c2f71d4e660a
Haleym Hidalgo Moyetones <109298016+hahidalgo@users.noreply.github.com>
2022-09-25 12:35:30 -0300

    Primer Commit del ejercicio

---
+++ App.py
@@ -0,0 +1,55 @@
+from flask import Flask, render_template, request, redirect, url_for, flash
+from flask_mysqldb import MySQL
+
+app = Flask(__name__)
+
+# Variables de conexion de la DB
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_PORT'] = 3306
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'python_flask_appcontact'
+mysql = MySQL(app)
+
+# settings SESSION
+app.secret_key = "mysecretkey"
+
+@app.route('/')
+def Index():
+    contact = mysql.connection.cursor()
+    contact.execute('SELECT * FROM contacts')
+    data = contact.fetchall()
+    contact.close()
+    return render_template('index.html', contacts = data)
+
+@app.route('/add', methods=['POST'])
+def add_contact():
+    if request.method == 'POST':
+        fullname = request.form['fullname']
+        email = request.form['email']
+        phone = request.form['phone']
+
+        contact = mysql.connection.cursor()
+        contact.execute(
+            "INSERT INTO contacts (fullname, phone, email) VALUES (%s,%s,%s)", 
+            (fullname, phone, email)
+            )
+        mysql.connection.commit()
+        contact.close()
+        flash('EL Contacto se ha registrado satisfactoriamente')
+        return redirect(url_for('Index'))
+
+@app.route('/edit')
+def edit_contact():
+    return 'Editar Contacto'
+
+@app.route('/delete/<string:id>', methods=['POST', 'GET'])
+def delete_contact(id):
+    contact = mysql.connection.cursor()
+    contact.execute('DELETE FROM contacts WHERE id = {0}'.format(id))
+    mysql.connection.commit()
+    flash('EL Contacto se ha ELIMINADO satisfactoriamente')
+    return redirect(url_for('Index'))
+
+if __name__ == '__main__':
+    app.run(port = 3000, debug = True)
